92fda36d42ca7dd25a49c0e7cf6e4f82da87146a
Sabbisetti123 <sahithsabbisetti@gmail.com>
2022-10-27 14:56:27 +0530

    added water

--- sahit.py
+++ sahit.py
@@ -2,3 +2,8 @@ hungry=input("enter are you hungry")
 if hungry=="yes":
      print("eat bread")
      print("eat samosa")
+else:
+     thirsty=input("enter are you thirsty")
+     if thirsty=="yes":
+         print("drink water")
+         
